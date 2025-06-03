11194e5461dcea7da2ac26baca8326a47ae1d041
HridaySaikia <hridayanandasaikia96@gmail.com>
2025-06-03 12:51:29 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null 
+// undefined
+// symbol =>unique
+
+// object
+
+
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof "hriday");
