995b60b01a467d155a5c2f392f2eb445022696d7
HridaySaikia <hridayanandasaikia96@gmail.com>
2025-06-04 07:22:14 +0000

    conversion of operations

--- 01_basics/03_conv_operations.js
+++ 01_basics/03_conv_operations.js
@@ -1,11 +1,11 @@
 let score = "33abd"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber )
-console.log( valueInNumber );
+// console.log(typeof valueInNumber )
+// console.log( valueInNumber );
 
 
 // "33" => 33
@@ -14,7 +14,7 @@ console.log( valueInNumber );
 
 let isLoggedIn  = ""
 let boolIsLoggedIn = Boolean(isLoggedIn);
-console.log(boolIsLoggedIn);
+// console.log(boolIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -23,5 +23,21 @@ console.log(boolIsLoggedIn);
 
 let someNumber = 43
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ******************* operations ********************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue); 
+
+// console.log(3**2);
+// console.log(3%2);
+
+ 
+console.log("1"+2);
+console.log(1 + "2");
+console.log("1"+2+2);
+console.log(1+2+"2");
