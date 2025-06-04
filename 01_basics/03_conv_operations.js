bdbd3ff432d9041ea421e72a6240a923987e1769
HridaySaikia <hridayanandasaikia96@gmail.com>
2025-06-04 06:42:59 +0000

    conversion if datatypes

---
+++ 01_basics/03_conv_operations.js
@@ -0,0 +1,27 @@
+let score = "33abd"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber )
+console.log( valueInNumber );
+
+
+// "33" => 33
+// "33abc" => NaN(Not a Number)
+// true = 1; false=> 0
+
+let isLoggedIn  = ""
+let boolIsLoggedIn = Boolean(isLoggedIn);
+console.log(boolIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "wow" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
